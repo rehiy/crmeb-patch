8764ccfef5144a71fdef14267c43331c30fd6ccc
若海 <mail@anrip.com>
2020-05-25 16:05:17 +0800

    增加虚拟打印机驱动

---
+++ 虚拟打印机/crmeb/services/printer/storage/Virtual.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace crmeb\services\printer\storage;
+
+use think\facade\Config;
+
+/**
+ * Class Virtual
+ * @author 若海<https://github.com/anrip/crmeb-patch>
+ * @package crmeb\services\printer\storage
+ */
+class Virtual extends YiLianYun
+{
+    private $orderInfo;
+
+    /**
+     * 开始打印
+     * @return bool|mixed|string
+     * @throws \Exception
+     */
+    public function startPrinter()
+    {
+        if (!$this->printerContent) {
+            return $this->setError('Missing print');
+        }
+
+        $conf = Config::get('printer.stores.virtual', []);
+        $sdir = app()->getRuntimePath() . $conf['save_dir'];
+
+        is_dir($sdir) || mkdir($sdir, '0755');
+        $file = $this->orderInfo['order_id'] . 'html';
+
+        file_put_contents($sdir . $file, $this->setPrinterContent);
+    }
+
+    /**
+     * 设置打印内容
+     * @param array $config
+     * @return YiLianYun
+     */
+    public function setPrinterContent(array $config): self
+    {
+        $this->orderInfo = $config['orderInfo'];
+        return parent::setPrinterContent($config);
+    }
+}
